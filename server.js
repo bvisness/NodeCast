84bb1ca3fcb3f43c6b2365efdd78a2a997ce78a7
Ben Visness <bvisness@gmail.com>
2015-11-22 16:44:59 -0600

    Initial commit
    Basic HTML server with SCSS and Nunjucks.

---
+++ server.js
@@ -0,0 +1,50 @@
+var PATH_PUBLIC = "public";
+var PATH_SCSS = "scss/style.scss";
+
+// Require modules
+var express = require('express');
+var app = express();
+var nunjucks = require('nunjucks');
+var sass = require('node-sass');
+var fs = require('fs');
+
+// Configure Express
+app.use(express.static(PATH_PUBLIC));
+
+// Configure Nunjucks
+var templates = process.env.NODE_PATH ? (process.env.NODE_PATH + '/templates') : 'templates';
+nunjucks.configure(templates, {
+    autoescape: true,
+    cache: false,
+    express: app
+});
+
+// Compile SCSS
+sass.render({
+    file: PATH_SCSS,
+    outputStyle: 'compressed'
+}, function(err, result) {
+    if (err) return console.log(err);
+
+    fs.writeFile(PATH_PUBLIC + "/style.css", result.css, function(err) {
+        if (err) return console.log(err);
+    }); 
+});
+
+// Use Nunjucks as template engine
+app.set('view engine', 'nunjucks');
+
+// Routes
+app.get('/', function(req, res) {
+    res.render('index', {
+        'title': 'Index'
+    });
+});
+
+// Start server
+var server = app.listen(8081, function() {
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log("Example app listening at http://%s:%s", host, port);
+});
