fa39e6268ab881fa2299daf00c3c3453040e1266
Ben Visness <bvisness@gmail.com>
2016-02-28 21:14:07 -0600

    Add start of live view
    Has the center section basically done, except for the timer.

--- routes.js
+++ routes.js
@@ -1,4 +1,9 @@
 module.exports.addWebcastRoutes = function(app) {
+    app.get('/live', function(req, res) {
+        res.render('live', {
+            'title': 'Live'
+        });
+    });
     app.get('/tbaloader', function(req, res) {
         res.render('tbaloader', {
             'title': 'Load from The Blue Alliance'
