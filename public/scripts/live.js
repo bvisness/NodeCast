a43b8a4e6f57c8bf2e7dd46ff072288bd1ee5396
Ben Visness <bvisness@gmail.com>
2016-03-26 21:48:47 -0500

    Update timer behavior
    Should now be consistent with FIRST's timer. Also relies on an object
    called matchState which contains all necessary variables.

--- public/scripts/live.js
+++ public/scripts/live.js
@@ -2,20 +2,17 @@
 //
 // Durations of the different match periods, in seconds.
 // AUTO_LENGTH: Duration of autonomous.
-// PAUSE_LENGTH: Duration of the pause between auto and teleop.
 // TELEOP_LENGTH: The duration of teleop, including the endgame time.
 // WARNING_LENGTH: The duration of the warning period or endgame. This is considered part of teleop.
 
 var AUTO_LENGTH = 15;
-var PAUSE_LENGTH = 1;
-var TELEOP_LENGTH = 145;
-var WARNING_LENGTH = 20;
+var TELEOP_LENGTH = 135;
+var WARNING_LENGTH = 30;
 
 var MATCH_LENGTH = AUTO_LENGTH + TELEOP_LENGTH;
 
 var config;
-var matchRunning;
-var matchStartTime;
+var matchState;
 
 function setLeftRightColors(isRedLeft, flipScoreColors) {
     var leftColor = isRedLeft ? 'red' : 'blue';
@@ -59,31 +56,23 @@ function getConfig(callback = function(){}) {
 }
 
 function updateTimer() {
-    if (!matchRunning && !matchStartTime) {
-        $('.timer-count').text(MATCH_LENGTH);
+    // Quit early if no active match
+    if (matchState['period'] == 'inactive') {
+        $('.timer-count').text(AUTO_LENGTH);
         $('.timer-fill').css('width', 0);
         $('.timer-fill').removeClass('red yellow');
-    }
-
-    if (!matchRunning) {
         return;
     }
 
-    teleopStartTime = matchStartTime + AUTO_LENGTH + PAUSE_LENGTH;
-
-    var secondsFromStart = Math.floor((Date.now() - parseInt(matchStartTime)) / 1000);
-    var secondsFromTeleop = Math.floor((Date.now() - parseInt(teleopStartTime)) / 1000);
+    // Update timer count
+    var secondsFromAuto = Math.floor((Date.now() - parseInt(matchState['start_time'])) / 1000);
+    var secondsFromTeleop = Math.floor((Date.now() - parseInt(matchState['teleop_time'])) / 1000);
 
     var output;
-    if (isNaN(secondsFromTeleop)) {
-        if (secondsFromStart >= AUTO_LENGTH) {
-            output = MATCH_LENGTH - AUTO_LENGTH;
-        } else {
-            output = MATCH_LENGTH - secondsFromStart;
-        }
-    }
-    else {
-        output = MATCH_LENGTH - AUTO_LENGTH - secondsFromTeleop;
+    if (matchState['period'] == 'autonomous') {
+        output = AUTO_LENGTH - secondsFromAuto;
+    } else {
+        output = TELEOP_LENGTH - secondsFromTeleop;
     }
 
     if (output < 0) {
@@ -92,11 +81,21 @@ function updateTimer() {
 
     $('.timer-count').text(output);
 
-    timerWidth = ((MATCH_LENGTH - output) / MATCH_LENGTH) * 100;
+    // Update timer fill
+    var secondsFromMatchStart;
+    if (matchState['period'] == 'autonomous') {
+        secondsFromMatchStart = Math.min(secondsFromAuto, AUTO_LENGTH);
+    } else {
+        secondsFromMatchStart = Math.min(AUTO_LENGTH + secondsFromTeleop, MATCH_LENGTH);
+    }
+
+    timerWidth = (secondsFromMatchStart / MATCH_LENGTH) * 100;
     $('.timer-fill').css('width', timerWidth + "%");
-    if (output === 0) {
+
+    var secondsToMatchEnd = MATCH_LENGTH - secondsFromMatchStart;
+    if (secondsToMatchEnd === 0) {
         $('.timer-fill').addClass('red');
-    } else if (output <= WARNING_LENGTH) {
+    } else if (secondsToMatchEnd <= WARNING_LENGTH) {
         $('.timer-fill').addClass('yellow');
     } else {
         $('.timer-fill').removeClass('red yellow');
