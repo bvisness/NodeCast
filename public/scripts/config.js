8eb33ba5b64caf3dc00c9fd96ddcf0cc52ec9d11
Ben Visness <bvisness@gmail.com>
2016-04-06 01:05:11 -0500

    Initialize config if necessary

--- public/scripts/config.js
+++ public/scripts/config.js
@@ -1,12 +1,36 @@
 var config = {};
 
+function initializeConfig(callback = function(){}) {
+    $.ajax({
+        method: 'PUT',
+        url: '/api/config',
+        contentType: 'application/json',
+        data: JSON.stringify({
+            'event_key': '2016mnmi2',
+            'event_name': 'North Star Regional',
+            'is_red_left': true,
+            'flip_score_colors': true,
+            'max_tower_strength': 8,
+            'num_qualification_matches': 50
+        }),
+        success: function(data) {
+            console.log('Initialized config');
+            callback();
+        }
+    });
+}
+
 function getConfig(callback = function(){}) {
     $.ajax({
         method: 'GET',
         url: '/api/config',
         success: function(data) {
             config = data;
-            callback();
+            if (!config) {
+                initializeConfig(callback);
+            } else {
+                callback();
+            }
         }
     });
 }
